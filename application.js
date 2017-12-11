8f0831c790632873a788860cc65d76f90a9c476b
robdir <rhwickham@gmail.com>
2017-12-11 16:02:30 +0100

    filtering works

--- application.js
+++ application.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
   $('.tour').on('click','button', function() {
     var tour = $(this).closest('.tour');
     var discount = tour.data('discount');
-    var message = $('<span>Call 1-555-jquery-air for a $' + discount + ' discount.</span>');
+    var message = $('<span>' + '<br>' + 'Call 1-555-jquery-air for a $'
+     + discount + '  discount.</span>' +'<br>');
     tour.append(message);
     // $(this).remove();
   });
@@ -12,6 +13,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('#filters').on('click', '.on-sale', function(){
-    
+    var el = $('.tour').filter('.on-sale');
+    el.addClass('highlight');
   });
 });
